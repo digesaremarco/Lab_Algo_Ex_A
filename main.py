d01c1b0f6c52034d47171b82bbe44bb28788071b
digesaremarco <digesaremarco@gmail.com>
2023-11-13 19:52:50 +0100

    Created test for insert in linked list

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from timeit import default_timer as timer
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import LinkedList
 
 
-# Press the green button in the gutter to run the script.
+def testAddLinkedList(iterations, l, k):
+    times = []
+    elements = []
+    for i in range(iterations):
+        #sum = 0
+        start = timer()
+        for j in range(50):
+            l.add(k[(j)])
+        end = timer()
+            #sum += (end - start) * 1000  # convert seconds to milliseconds
+        #average_time = sum / 20
+        times.append((end-start)*1000)
+        elements.append(l.size())
+    plt.plot(elements, times)
+    plt.xlabel('Elementi nella lista')
+    plt.ylabel('Tempo di esecuzione (millisecondi)')
+    plt.title('Performance del metodo add')
+    plt.show()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
+    k = np.random.randint(0, 1000000, 1000) # generate 1000 random numbers between 0 and 1000000
+    l = LinkedList.LinkedList() # create an empty list
+    testAddLinkedList(2000, l, k)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
