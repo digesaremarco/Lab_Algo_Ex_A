0e28473600ab1259d9ed9972b2e07d3ad5c320d7
digesaremarco <digesaremarco@gmail.com>
2023-11-15 17:56:27 +0100

    Created test for Hash Table, but there is a clustering

--- HashTable.py
+++ HashTable.py
@@ -1,16 +1,21 @@
+import numpy as np
+
+
 class HashTable:
     def __init__(self, nitems):
-        self.size = nitems/0.8 # the size of the table is 20% bigger than the number of items, it gives a load factor of 0.8
-        self.table = [None] * self.size  # create an empty list
+        self.size = int(nitems/0.8) # the size of the table is 20% bigger than the number of items, it gives a load factor of 0.8
+        self.table = np.full(self.size, None) # initialize the table with None values
 
     def hashfunction(self, key, i):
-        return (key % self.size + i) % self.size
+        #return (key + i ** 2) % self.size # quadratic probing
+        return (key % self.size + i) % self.size # linear probing has problems with clustering
+
 
     def insert(self, key):
         i = 0
         while i < self.size:
             j = self.hashfunction(key, i)
-            if self.table[j] is None or self.table[j] == -1:
+            if self.table[j] is None:
                 self.table[j] = key
                 return j
             else:
@@ -19,11 +24,13 @@ class HashTable:
 
     def search(self, key):
         i = 0
-        while i < self.size or self.table[j] is not None or self.table[j] != -1: # search until the end of the table or until an empty cell is found
+        while i < self.size : # search until the end of the table or until an empty cell is found
             j = self.hashfunction(key, i)
             if self.table[j] == key:
                 return j
             i += 1
+            if self.table[j] is None or self.table[j] == -1:
+                break
         return None
 
     def remove(self, key):
@@ -33,3 +40,6 @@ class HashTable:
             return True
         else:
             return False
+
+    def getSize(self):
+        return self.size
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import LinkedList
+import HashTable
 
 
 def testAddLinkedList(iterations, k):
@@ -17,6 +18,7 @@ def testAddLinkedList(iterations, k):
         times.append((end - start) * 1000)
         elements.append(l.size())
     return times, elements
+
 
 def testSearchLinkedList(iterations, k):
     times = []
@@ -33,6 +35,7 @@ def testSearchLinkedList(iterations, k):
         elements.append(l.size())
     return times, elements
 
+
 def testLinkedList(k):
     timesAdd = []
     elementsAdd = []
@@ -44,11 +47,11 @@ def testLinkedList(k):
     avgElementsSearch = []
 
     # test add
-    for i in range (300):
+    for i in range(300):
         time, element = testAddLinkedList(100, k)
         timesAdd.append(time)
         elementsAdd.append(element)
-    for i in range(len(timesAdd[0])): # calculate the average time and the average number of elements for each iteration
+    for i in range(len(timesAdd[0])):
         avgTimesAdd.append(0)
         avgElementsAdd.append(0)
         for j in range(len(timesAdd)):
@@ -63,11 +66,11 @@ def testLinkedList(k):
     plt.show()
 
     # test search
-    for i in range (10):
+    for i in range(300):
         time, element = testSearchLinkedList(10, k)
         timesSearch.append(time)
         elementsSearch.append(element)
-    for i in range(len(timesSearch[0])): # calculate the average time and the average number of elements for each iteration
+    for i in range(len(timesSearch[0])):
         avgTimesSearch.append(0)
         avgElementsSearch.append(0)
         for j in range(len(timesSearch)):
@@ -82,7 +85,89 @@ def testLinkedList(k):
     plt.show()
 
 
+def testAddHashTable(iterations, k):
+    times = []
+    elements = []
+    h = HashTable.HashTable(len(k))
+    for i in range(iterations):
+        start = timer()
+        for j in range(10):
+            h.insert(k[(j + i * 10) % len(k)])
+        end = timer()
+        times.append((end - start) * 1000)
+        elements.append(i * 10)
+    return times, elements
+
+
+def testSearchHashTable(iterations, k):
+    times = []
+    elements = []
+    h = HashTable.HashTable(len(k))
+    for i in range(iterations):
+        for j in range(100):
+            h.insert(k[(j + i * 10) % len(k)])
+        elements.append(j * (i+1))
+        start = timer()
+        for j in range(10):
+            h.search(random.randint(0, 1000))
+        end = timer()
+        times.append((end - start) * 1000)
+    return times, elements
+
+
+def testHashTable(k):
+    timesAdd = []
+    elementsAdd = []
+    timesSearch = []
+    elementsSearch = []
+    avgTimesAdd = []
+    avgElementsAdd = []
+    avgTimesSearch = []
+    avgElementsSearch = []
+
+    # test add
+    for i in range(300):
+        time, element = testAddHashTable(100, k)
+        timesAdd.append(time)
+        elementsAdd.append(element)
+    for i in range(len(timesAdd[0])):
+        avgTimesAdd.append(0)
+        avgElementsAdd.append(0)
+        for j in range(len(timesAdd)):
+            avgTimesAdd[i] += timesAdd[j][i]
+            avgElementsAdd[i] += elementsAdd[j][i]
+        avgTimesAdd[i] /= len(timesAdd)
+        avgElementsAdd[i] /= len(timesAdd)
+    plt.plot(avgElementsAdd, avgTimesAdd)
+    plt.xlabel('Elementi nella tabella hash')
+    plt.ylabel('Tempo di esecuzione (millisecondi)')
+    plt.title('Performance inserimento in tabella hash')
+    plt.show()
+
+    # test search
+    for i in range(100):
+        time, element = testSearchHashTable(10, k)
+        timesSearch.append(time)
+        elementsSearch.append(element)
+    for i in range(len(timesSearch[0])):
+        avgTimesSearch.append(0)
+        avgElementsSearch.append(0)
+        for j in range(len(timesSearch)):
+            avgTimesSearch[i] += timesSearch[j][i]
+            avgElementsSearch[i] += elementsSearch[j][i]
+        avgTimesSearch[i] /= len(timesSearch)
+        avgElementsSearch[i] /= len(timesSearch)
+    plt.plot(avgElementsSearch, avgTimesSearch)
+    plt.xlabel('Elementi nella tabella hash')
+    plt.ylabel('Tempo di esecuzione (millisecondi)')
+    plt.title('Performance ricerca in tabella hash')
+    plt.show()
+
+
 if __name__ == '__main__':
     print('PyCharm')
-    k = np.random.randint(0, 1000000, 1000)  # generate 1000 random numbers between 0 and 1000000
-    testLinkedList(k)
+    h = np.random.randint(0, 100000, 1000)  # generate 3000 random numbers between 0 and 1000
+    #testLinkedList(k)
+    k = np.random.choice(10000, size=1000, replace=False)
+    testHashTable(k)
+
